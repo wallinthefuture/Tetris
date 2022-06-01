7df62cdfe0bb7c6209af23faf226d2532db4a2c6
Ivan <wallinthefuture@gmail.com>
2022-06-01 23:44:03 +0300

    add another figure and colors for this figure

--- src/game.js
+++ src/game.js
@@ -4,15 +4,9 @@ export default class Game {
   level = 0;
   playfield = this.createPlayField();
 
-  activePiece = {
-    x: 0,
-    y: 0,
-    blocks: [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 0, 0],
-    ],
-  };
+  activePiece = this.createPiece();
+
+  nextPiece = this.createPiece();
 
   getState() {
     const playfield = this.createPlayField();
@@ -52,6 +46,70 @@ export default class Game {
     return playfield;
   }
 
+  createPiece() {
+    const index = Math.floor(Math.random() * 7);
+    const type = 'IJLOSTZ'[index];
+    const piece = { x: 0, y: 0 };
+
+    switch (type) {
+      case 'I':
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [1, 1, 1, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ];
+        break;
+      case 'J':
+        piece.blocks = [
+          [0, 0, 0],
+          [2, 2, 2],
+          [0, 0, 2],
+        ];
+        break;
+      case 'L':
+        piece.blocks = [
+          [0, 0, 0],
+          [3, 3, 3],
+          [3, 0, 0],
+        ];
+        break;
+      case 'O':
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [0, 4, 4, 0],
+          [0, 4, 4, 0],
+          [0, 0, 0, 0],
+        ];
+        break;
+      case 'S':
+        piece.blocks = [
+          [0, 0, 0],
+          [0, 5, 5],
+          [5, 5, 0],
+        ];
+        break;
+      case 'T':
+        piece.blocks = [
+          [0, 0, 0],
+          [6, 6, 6],
+          [0, 6, 0],
+        ];
+        break;
+      case 'Z':
+        piece.blocks = [
+          [0, 0, 0],
+          [7, 7, 0],
+          [0, 7, 7],
+        ];
+        break;
+
+      default:
+        throw new Error('Неизвестный тип фигур');
+    }
+    return piece;
+  }
+
   movePieceLeft() {
     this.activePiece.x -= 1;
 
@@ -74,6 +132,7 @@ export default class Game {
     if (this.hasCollision()) {
       this.activePiece.y -= 1;
       this.lockPiece();
+      this.updatePieces();
     }
   }
 
@@ -126,5 +185,10 @@ export default class Game {
         }
       }
     }
+  }
+
+  updatePieces() {
+    this.activePiece = this.nextPiece;
+    this.nextPiece = this.createPiece();
   }
 }
