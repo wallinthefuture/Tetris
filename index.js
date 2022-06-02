78e4e7ef5a56dd9548acd7177d3c17452e0aa1e9
Ivan <wallinthefuture@gmail.com>
2022-06-02 11:32:00 +0300

    add up and right panel, add function to show a next figure

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import View from './src/view.js';
 const container = document.querySelector('.container');
 
 const game = new Game();
-const view = new View(container, 758, 428, 10, 20);
+const view = new View(container, 848, 548, 10, 20);
 
 const canvas = document.querySelector('canvas');
 canvas.classList.add('canvas');
@@ -32,3 +32,5 @@ document.addEventListener('keydown', (event) => {
       break;
   }
 });
+
+view.render(game.getState());
--- src/view.js
+++ src/view.js
@@ -20,22 +20,38 @@ export default class View {
     this.canvas.height = this.height;
     this.context = this.canvas.getContext('2d');
 
-    this.blockWidth = this.width / columns;
-    this.blockHeight = this.height / rows;
+    this.playfieldX = 0;
+    this.playfieldY = 0;
+    this.playfieldWidth = (this.width * 332) / 374;
+    this.playfieldHeight = (this.height * 82.6) / 107;
 
+    this.blockWidth = this.playfieldWidth / columns;
+    this.blockHeight = this.playfieldHeight / rows;
+
+    this.panelUpX = 0;
+    this.panelUpY = 0;
+    this.panelRightX = this.playfieldWidth + 10;
+    this.panelRightY = 0;
+    this.panelUpWidth = this.width;
+    this.panelUpHeight = (this.height * 30) / 107;
+    this.panelRightWidth = (this.width * 45) / 374;
+    this.panelRightHeight = this.height;
+    this.blockNextWidth = this.panelRightWidth / 5.8;
+    this.blockNextHeight = this.panelRightHeight / 26;
     this.element.appendChild(this.canvas);
   }
 
-  render({ playfield }) {
+  render(state) {
     this.clearScreen();
-    this.renderPlayField(playfield);
+    this.renderPlayField(state);
+    this.renderPanel(state);
   }
 
   clearScreen() {
     this.context.clearRect(0, 0, this.width, this.height);
   }
 
-  renderPlayField(playfield) {
+  renderPlayField({ playfield }) {
     for (let y = 0; y < playfield.length; y++) {
       const line = playfield[y];
 
@@ -45,9 +61,50 @@ export default class View {
         if (block) {
           this.renderBlock(
             x * this.blockWidth,
-            y * this.blockHeight,
+            this.panelUpHeight - 30 + y * this.blockHeight,
             this.blockWidth,
             this.blockHeight,
+            View.colors[block]
+          );
+        }
+      }
+    }
+  }
+
+  renderPanel({ level, score, lines, nextPiece }) {
+    this.context.textAlign = 'start';
+    this.context.textBaseline = 'top';
+    this.context.fillStyle = 'white';
+    this.context.font = '14px "Press Start 2P"';
+
+    this.context.fillText(
+      `Score: ${score}`,
+      this.panelUpX + 255,
+      this.panelUpY + 20
+    );
+    this.context.fillText(
+      `Lines: ${lines}`,
+      this.panelUpX + 255,
+      this.panelUpY + 44
+    );
+    this.context.fillText(
+      `Level: ${level}`,
+      this.panelUpX + 255,
+      this.panelUpY + 68
+    );
+    this.context.fillText(`Next`, this.panelRightX + 8, this.panelRightY + 160);
+
+    for (let y = 0; y < nextPiece.blocks.length; y++) {
+      for (let x = 0; x < nextPiece.blocks[y].length; x++) {
+        const block = nextPiece.blocks[y][x];
+
+        if (block) {
+          this.renderBlock(
+            this.panelRightX + x * this.blockNextWidth + 2,
+            this.panelRightY + 170 + y * this.blockNextHeight,
+            this.blockNextWidth,
+            this.blockNextHeight,
+
             View.colors[block]
           );
         }
